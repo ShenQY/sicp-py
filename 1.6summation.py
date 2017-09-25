eb5be4ca4c4bc8e233e1e6998004f643afbb2429
ShenQY <157072582@qq.com>
2017-09-25 15:45:41 +0800

    add 高阶函数case

---
+++ 1.6summation.py
@@ -0,0 +1,26 @@
+#encoding:utf-8
+#高阶函数,将函数作为参数值传递
+def summation(n,term,next):
+	total,k = 0,1
+	while k <= n:
+		total , k = total + term(k) , next(k)
+	return total
+
+def identify(k):
+	return k
+#返回n的3次方
+def cube(n):
+	return pow(n,3)
+
+def successor(k):
+	return k + 1
+
+#求自然数的和:1,2,3,4,5
+def sum_naturals(n):
+	return summation(n,identify,successor)
+#求立方的和:1 + 2^3 + 3^3 + 4^3......
+def sum_cubes(n):
+	return summation(n,cube,successor)
+
+print sum_naturals(3)
+print sum_cubes(3)
