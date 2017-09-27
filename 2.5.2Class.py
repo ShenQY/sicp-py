d5c0b66676d0c65b8251ee25d1b940b2efc70460
ShenQY <157072582@qq.com>
2017-09-27 18:05:11 +0800

    add class muinherit,add instace implement

---
+++ 2.5.2Class.py
@@ -0,0 +1,37 @@
+#encoding:utf-8
+class Account(object):
+	"""docstring for Account"""
+	interest = 0
+	def __init__(self,holder):
+		super(Account, self).__init__()
+		self.balance = 0
+		self.holder = holder
+	def publicMethod(self):
+		print ('public')
+
+class BAccount(Account):
+	"""docstring for BAccount"""
+	def __init__(self, arg):
+		super(BAccount, self).__init__()
+		self.arg = arg
+class DAccount(BAccount):
+	"""docstring for DAccount"""
+	def __init__(self, arg):
+		super(DAccount, self).__init__()
+		self.arg = arg
+		
+class CAccount(DAccount,BAccount):
+	"""docstring for CAccount"""
+	def __init__(self, arg):
+		super(CAccount, self).__init__()
+		self.arg = arg
+		
+a = Account('jim')
+a.publicMethod()
+
+b = Account('mali')
+a.interest = 1
+print (Account.interest,a.interest,b.interest)
+Account.interest = 3
+print (Account.interest,a.interest,b.interest)
+print ([c.__name__ for c in CAccount.mro()])
---
+++ 2.6.1make_instance.py
@@ -0,0 +1,23 @@
+#encoding:utf-8
+def make_instace(cls):
+	"""Return a new object instance,which is a dispatch dictionary"""
+	def get_value(name):
+		if name in attributes:
+			return attributes[name]
+		else:
+			value = cls['get'](name)
+			return bind_method(value,instance)
+	def set_value(name,value):
+		attributes[name] = value
+	attributes = {}
+	instance = {'get':get_value,'set':set_value}
+	return instance
+
+def bind_method(value,instace):
+	"""Return a bound method if value is callable,or other value"""
+	if callable(value):
+		def method(*ages):
+			return value(instance,*args)
+		return method
+	else:
+		return value
